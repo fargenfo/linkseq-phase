31a535fd46bf0a5d7dfc40e729c8c6b18282e770
Ólavur Mortensen <olavurmortensen@gmail.com>
2021-05-07 09:38:51 +0100

    Fixed indenting in script.

--- bin/remove_format_field.py
+++ bin/remove_format_field.py
@@ -9,9 +9,9 @@ parser = argparse.ArgumentParser(description='Render Jinja2 template.')
 
 # Add the arguments.
 parser.add_argument('--vcf', type=str,
-                    help='Path to VCF file.')
+        help='Path to VCF file.')
 parser.add_argument('--field', type=str,
-                    help='Name of field to remove from format.')
+        help='Name of field to remove from format.')
 
 # Parse script input arguments.
 args = parser.parse_args()
@@ -30,40 +30,40 @@ with open(vcf) as fid:
             if line[:8] == '##FORMAT' and 'ID=' + field in line:
                 format_line_removed = True
                 continue
-                
+
             print(line, end='')
         else:
             # Remove trailing whitespace from line.
             line = line.strip()
-            
+
             # Split the line into columns.
             columns = line.split('\t')
-            
+
             # The 8th column is the FORMAT column.
             format_column = columns[8]
-            
+
             # The sample (or genotype) columns.
             sample_columns = columns[9:]
-           
+
             # Start building the new row.
             new_row = columns[:8]
-            
+
             if field in format_column:
                 # Split the FORMAT column into individual fields which are separated by colons.
                 format_fields = format_column.split(':')
-                
+
                 # Get the index of the relevant field in the list.
                 field_idx = format_fields.index(field)
-                
+
                 # Remove this field from the list of fields.
                 format_fields.pop(field_idx)
-                
+
                 # Join the fields with colons again.
                 new_format_column = ':'.join(format_fields)
-                
+
                 # Start making the new line.
                 new_row.append(new_format_column)
-                
+
                 # Remove this field from each of the samples genotype fields.
                 for sample_column in sample_columns:
                     # Split the column by colon into fields.
@@ -74,14 +74,14 @@ with open(vcf) as fid:
                     new_sample_fields = ':'.join(sample_fields)
                     # Append the column to the other columns.
                     new_row.append(new_sample_fields)
-                
+
                 # Make the new line by joining all the columns by tabs.
                 new_line = '\t'.join(new_row)
             else:
                 # If the relevant field is not present in the line, just print the line.
                 new_line = line
-            
+
             print(new_line)
-            
+
 if not format_line_removed:
     logging.warning('A FORMAT line matching the {field} field was not found in the header of the VCF'.format(field=field))
